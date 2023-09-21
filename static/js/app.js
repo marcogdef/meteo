6a8d5d00e8c06fde50643f74f867ae061dfd345a
marcogdef <116938984+marcogdef@users.noreply.github.com>
2023-09-21 10:33:42 +0200

    3

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function map(latitude, longitude){
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    }).addTo(map);
+}
+
+map(50.45, 10.45)
 
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault()
